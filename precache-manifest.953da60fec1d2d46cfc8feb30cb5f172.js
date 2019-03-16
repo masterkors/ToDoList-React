0a6f414e96669307901992b1d4e9e4df757b1db1
robak <marek.kors@wp.pl>
2019-03-16 17:12:09 +0100

    Updates

---
+++ precache-manifest.953da60fec1d2d46cfc8feb30cb5f172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfaa32f1797b435fc275",
+    "url": "/ToDoList-React/static/js/runtime~main.88779d92.js"
+  },
+  {
+    "revision": "9029bc6efb119b2bddb9",
+    "url": "/ToDoList-React/static/js/main.a086e3e6.chunk.js"
+  },
+  {
+    "revision": "ce6ab34a656d35debfc6",
+    "url": "/ToDoList-React/static/js/2.7ade61ff.chunk.js"
+  },
+  {
+    "revision": "9029bc6efb119b2bddb9",
+    "url": "/ToDoList-React/static/css/main.e9cafb44.chunk.css"
+  },
+  {
+    "revision": "ec0f70802aa53e022f3383f68aaa2f03",
+    "url": "/ToDoList-React/index.html"
+  }
+];
